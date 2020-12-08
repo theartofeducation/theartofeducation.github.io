1e84d259c6832b159b208e5b59abfc6c8124cc83
Bob Yexley <bob@yexley.net>
2020-12-08 16:37:39 -0500

    feature(ch24): added links to engineering docs and npm org (#6)

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import { FormattedMessage, useIntl } from "gatsby-plugin-intl"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { ExternalLink } from "../components/external-link"
+import { ExternalLink } from "@aoeu/external-link"
 import { Container } from "../components/container"
-import { List, ListItem } from "../components/list"
+import { List, ListItem } from "@aoeu/list"
 import { Svg } from "../components/svgs"
 import { Card } from "../components/card"
 import "./home.scss"
@@ -34,18 +34,62 @@ const IndexPage = () => {
         <h2><FormattedMessage id="pages.index.resources-heading" /></h2>
         <List orientation="centered" className="resource-list">
           <ListItem>
-            <Card className="resource-card" linkTo="https://github.com/theartofeducation">
+            <Card className="resource-card" linkTo="https://github.com/theartofeducation/ui-common">
               <header><FormattedMessage id="pages.index.style-guide-header" /></header>
               <article><FormattedMessage id="pages.index.style-guide-content" /></article>
               <footer>
                 <List orientation="right">
                   <ListItem>
-                    <ExternalLink url="https://github.com/theartofeducation/react-component-library">
+                    <ExternalLink
+                      url="https://github.com/theartofeducation/ui-common">
                       <Svg symbolId="github-icon" className="link-icon" />
                     </ExternalLink>
                   </ListItem>
                   <ListItem>
-                    <ExternalLink url="https://theartofeducation.github.io/react-component-library">
+                    <ExternalLink
+                      url="https://theartofeducation.github.io/ui-common">
+                      <Svg symbolId="external-link-icon" className="link-icon" />
+                    </ExternalLink>
+                  </ListItem>
+                </List>
+              </footer>
+            </Card>
+          </ListItem>
+          <ListItem>
+            <Card
+              className="resource-card"
+              linkTo="https://github.com/theartofeducation/engineering-docs">
+              <header><FormattedMessage id="pages.index.eng-docs-header" /></header>
+              <article><FormattedMessage id="pages.index.eng-docs-content" /></article>
+              <footer>
+                <List orientation="right">
+                  <ListItem>
+                    <ExternalLink
+                      url="https://github.com/theartofeducation/engineering-docs">
+                      <Svg symbolId="github-icon" className="link-icon" />
+                    </ExternalLink>
+                  </ListItem>
+                  <ListItem>
+                    <ExternalLink
+                      url="https://theartofeducation.github.io/engineering-docs">
+                      <Svg symbolId="external-link-icon" className="link-icon" />
+                    </ExternalLink>
+                  </ListItem>
+                </List>
+              </footer>
+            </Card>
+          </ListItem>
+          <ListItem>
+            <Card
+              className="resource-card"
+              linkTo="https://npmjs.com/org/aoeu">
+              <header><FormattedMessage id="pages.index.npm-org-header" /></header>
+              <article><FormattedMessage id="pages.index.npm-org-content" /></article>
+              <footer>
+                <List orientation="right">
+                  <ListItem>
+                    <ExternalLink
+                      url="https://npmjs.com/org/aoeu">
                       <Svg symbolId="external-link-icon" className="link-icon" />
                     </ExternalLink>
                   </ListItem>
